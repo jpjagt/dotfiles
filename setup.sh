b62418fbfb8a42882be5f9bc0cdd431abfda2e69
jeroen <jpjagt@pm.me>
2022-05-20 16:10:24 +0200

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
+DOTFILES_DIRECTORY=$(cd `dirname $0` && pwd)
+
+ln -s $DOTFILES_DIRECTORY/.emacs.d ~/.emacs.d
+ln -s $DOTFILES_DIRECTORY/zshrc ~/.zshrc
+
 # global gitignore
-rm ~/.gitignore_global
-ln -s ~/code/jpjagt/dotfiles/.gitignore_global ~/.gitignore_global
+ln -s $DOTFILES_DIRECTORY/.gitignore_global ~/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
